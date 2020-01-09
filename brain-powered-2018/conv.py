35a9159229be223353d9a3adf3419d5aaa2504dc
Kenan <kenanamat@gmail.com>
2020-01-09 14:19:44 +0100

    Ik wil ook alle bestanden

--- brain-powered-2018/conv.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-data = np.load('KLAAS.npy', allow_pickle=True)
-data[0][0].tofile("data.csv",format='%.4e',sep=',')
--- brain-powered-2018/npy.py
+++
@@ -1,8 +0,0 @@
-import scipy.io as sio
-import numpy as np
-data = sio.loadmat('KLAAS.mat')
-np.save('klaas.npy', data)
-
-
-data = np.load('klaas.npy',allow_pickle=True).T
-print(data)
